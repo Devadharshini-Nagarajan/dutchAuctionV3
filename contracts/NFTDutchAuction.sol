22703e9e7dc6f2285a1f1508303f878b87e24c40
Devadharshini-Nagarajan <nagarajan.de@northeastern.edu>
2023-06-21 20:48:18 -0400

    feat: nft dutch auction

--- contracts/NFTDutchAuction.sol
+++ contracts/NFTDutchAuction.sol
@@ -4,14 +4,12 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NFTDutchAuction {
-    using Counters for Counters.Counter;
     using SafeMath for uint256;
 
-    IERC721 public NFT_address;
-    uint256 public NFT_id;
+    IERC721 public NFTAddress;
+    uint256 public NFTId;
     uint256 public auctionStartBlock;
     uint256 public reservePrice;
     uint256 public numBlocksAuctionOpen;
@@ -19,38 +17,42 @@ contract NFTDutchAuction {
     uint256 public initialPrice;
     uint256 public auctionEndBlock;
     address payable public owner;
-    bool private ended;
+    bool private isAuctionOver;
 
-    modifier isAuctionValid() {
-        require(msg.sender != owner, "owner can't bid");
-        require(ended == false, "auction is ended");
-        require(block.timestamp > auctionStartBlock, "auction is not started yet");
-        require(block.timestamp < auctionEndBlock, "auction is ended");
-        _;
-    }
-    constructor(address erc721TokenAddress, uint256 _nftTokenId,uint256 _reservePrice, uint256 _numBlocksAuctionOpen, uint256 _offerPriceDecrement) {
-        NFT_address = IERC721(erc721TokenAddress);
-        NFT_id = _nftTokenId;
+    constructor(
+        address erc721TokenAddress,
+        uint256 _nftTokenId,
+        uint256 _reservePrice,
+        uint256 _numBlocksAuctionOpen,
+        uint256 _offerPriceDecrement
+    ) {
+        NFTAddress = IERC721(erc721TokenAddress);
+        NFTId = _nftTokenId;
         reservePrice = _reservePrice;
         numBlocksAuctionOpen = _numBlocksAuctionOpen;
         offerPriceDecrement = _offerPriceDecrement;
-        initialPrice = reservePrice.add(numBlocksAuctionOpen.mul(offerPriceDecrement));
-        auctionStartBlock = block.timestamp;
-        auctionEndBlock = block.timestamp.add(_numBlocksAuctionOpen);
+        initialPrice = reservePrice.add(
+            numBlocksAuctionOpen.mul(offerPriceDecrement)
+        );
+        auctionStartBlock = block.number;
+        auctionEndBlock = block.number + numBlocksAuctionOpen;
         owner = payable(msg.sender);
-        ended = false;
-
+        isAuctionOver = false;
     }
 
-    function bid() isAuctionValid() public payable returns(address)  {
-        uint256 blockPassed = block.timestamp.sub(auctionStartBlock);
-        uint256 currentPrice = initialPrice.sub(blockPassed.mul(offerPriceDecrement));
-        if (msg.value >= currentPrice){
-            NFT_address.safeTransferFrom(owner,msg.sender,NFT_id);
+    function placeBid() public payable returns (address) {
+        require(msg.sender != owner, "Owner can't bid");
+        require(isAuctionOver == false, "Auction is ended!");
+        require(block.number <= auctionEndBlock, "Auction is ended!");
+
+        uint256 goneBlocks = auctionEndBlock - block.number;
+        uint256 currentPrice = initialPrice - goneBlocks * offerPriceDecrement;
+
+        if (msg.value >= currentPrice) {
+            NFTAddress.safeTransferFrom(owner, msg.sender, NFTId);
             owner.transfer(msg.value);
-            ended = true;
-        }
-        else {
+            isAuctionOver = true;
+        } else {
             address payable bidder = payable(msg.sender);
             bidder.transfer(msg.value);
         }
---
+++ test/sample.ts
@@ -0,0 +1,4 @@
+// it("Is auction contract approved for transfering NFT", async function () {
+//     const { dutch_nft,nft_id,basic_dutch_auction} = await loadFixture(deployNFTDutchAuction);
+//     expect(await dutch_nft.getApproved(nft_id)).to.equal(basic_dutch_auction.address)
+//   });
